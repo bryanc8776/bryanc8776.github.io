c383f675a2b45e1de8b1b42d01c69aed79a044a0
bryanc8776 <146866834+bryanc8776@users.noreply.github.com>
2023-10-03 12:45:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Bryan"; // update the part BETWEEN the "quotes"
+let userName = "bryanc8776"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
